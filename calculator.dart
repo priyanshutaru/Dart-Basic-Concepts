94d1fe547d407369cf3ebc76e179b7f2075dc390
LENV <technicalpriyanshutaru@gmail.com>
2023-05-01 13:17:35 +0530

    caculator code is done.

---
+++ calculator.dart
@@ -0,0 +1,16 @@
+void main(){
+
+  var a = 10;
+  var b=10.5;
+
+  var addition=a+b;
+  var subtraction= a-b;
+  var multiplication = a*b;
+  var division = a/b;
+
+  print("Addition of the a and b is $addition\n");
+  print("Subtraction of the a and b is $subtraction\n");
+  print("Multiplication of the a and b is $multiplication\n");
+  print("Division of the a and b is $division\n");
+
+}
--- datatypes_and_variables.dart
+++ datatypes_and_variables.dart
@@ -1,3 +1,44 @@
 void main(){
-  print("hello World");
+  // print("hello World");
+  // print("Hy This is My First Program");
+
+  var name= "Priyanshu";
+  // print(name);
+  print("My name is $name");//this is called the string interpolation.
+
+  var lastname= "Kumar";
+  print("My name is $name $lastname");
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
