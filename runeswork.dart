4cd3b159cb5dba86d0a7b59886380432194ad06f
LENV <technicalpriyanshutaru@gmail.com>
2023-05-01 21:13:36 +0530

    runes work completed.

---
+++ runeswork.dart
@@ -0,0 +1,10 @@
+void main(){
+
+  // this is give us the unicode of the strings as you can see in the example.
+
+  var valOne = "a";
+  var valTwo= "A";
+  print(valOne.runes);
+  print(valTwo.runes);
+  
+}
