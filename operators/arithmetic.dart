88a0d690f4abc9881147eeeb93a965707d8bcdec
LENV <technicalpriyanshutaru@gmail.com>
2023-05-02 10:15:25 +0530

    Arithmetic Operator is done.

---
+++ operators/arithmetic.dart
@@ -0,0 +1,27 @@
+void main(){
+  int numOne = 20;
+  int numtwo = 10;
+
+  int sum = numOne+numtwo;
+  int sub = numOne-numtwo;
+  int multi = numOne*numtwo;
+  int div = numOne*numtwo;
+  int moduls = numOne%numtwo;
+
+
+  //Display the output.
+
+
+  print("the addition of two number is $sum");
+  print("the subtraction of two number is $sub");
+  print("the multiplication of two number is $multi");
+  print("the division of two number is $div");
+  print("the moduls of two number is $moduls");
+  
+  
+
+
+
+
+
+}
