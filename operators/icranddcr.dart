b0532ca6b2172be1da67081704396cbc3241ee83
LENV <technicalpriyanshutaru@gmail.com>
2023-05-02 10:24:43 +0530

    Icrement and dcrement is done.

---
+++ operators/icranddcr.dart
@@ -0,0 +1,30 @@
+void main(){
+
+  int num1=25;
+
+  num1++;  // num=num+1;
+  print(num1); //26
+  ++num1;  // num=num+1;
+  print(num1);//27
+  
+  int num2= 20;
+  
+  num2--; // num2 = num2-1;
+  print(num2);//19
+  --num2;  //num2 = num2-1;
+  print(num2);//18
+
+  int a = num1;
+  int b = num2;
+
+
+  print(a++);
+  print(++a);
+  print(b--);
+  // int c=b;
+  // print(c);
+  print(--b);
+
+
+
+}
