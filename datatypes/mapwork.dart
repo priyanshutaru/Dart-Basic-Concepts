c77bc604d437063ee826a9c481175ecf960d3330
LENV <technicalpriyanshutaru@gmail.com>
2023-05-02 09:14:36 +0530

    map work is done.

---
+++ datatypes/mapwork.dart
@@ -0,0 +1,13 @@
+void main(){
+  //maps take the key value and the first one is string is mandatory
+  Map<String,String> countryCapital = {
+
+    "India": "New Delhi",
+    "Nepal": "Kathmandu",
+    "USA" : "WDC",
+
+  };
+
+  print(Map().length);
+  print(countryCapital);
+}
