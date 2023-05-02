8b2ed582ad66000251df76a2d405741d344837ab
LENV <technicalpriyanshutaru@gmail.com>
2023-05-02 09:19:26 +0530

    Language types is compleated.

---
+++ datatypes/types.dart
@@ -0,0 +1,16 @@
+void main(){
+
+  // dart is optinally types language it can behave like statucally type or dynamically typed 
+
+  //static type example
+
+  var name = "Priyanshu";
+   //name = 40;//it through the error just because it behave like the statically type lanuage
+
+  print(name);
+
+   dynamic nameOne = "Priyanshu";
+   nameOne = "1705";//it not through the error just because it behave like the dynamically type lanuage
+  
+  print(name);
+}
