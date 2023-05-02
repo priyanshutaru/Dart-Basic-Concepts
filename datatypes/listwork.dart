e54f1e4bf231b1c145a2ab9989a5047ccb065bfc
LENV <technicalpriyanshutaru@gmail.com>
2023-05-02 08:52:07 +0530

    List is done.

---
+++ datatypes/listwork.dart
@@ -0,0 +1,13 @@
+void main(){
+
+  List<String> countries = ["India", "Usa","Japan"];//correct way to right list
+
+  List<String> countries2 = ["India, USA , Japan"];//incorrect way to right list
+
+  print(countries.length);
+  print(countries[0]);
+  print(countries[1]);
+  print(countries2.length);
+  // print(countries2[0]);
+  // print(countries2[2]);
+}
