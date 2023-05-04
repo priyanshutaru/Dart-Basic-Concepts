d858ff2643036b92badf2e6adfcb924e486f6360
LENV <technicalpriyanshutaru@gmail.com>
2023-05-04 12:14:13 +0530

    ternory is completed.

--- conditional/ternory.dart
+++ conditional/ternory.dart
@@ -1,5 +1,19 @@
 void main(){
 
-  print("Hello Priyanshu Welcome after two days.");
+  var age = 30;
+
+  // if(age>=13 && age<=19){
+  //   print("You are a teenager");
+  // }
+  // else{
+  //   print("Your are not teenager");
+  // }
+
+ // age>=13 && age<=19 ? print("You are teenager with age of $age"):print("Your are not teenager with age of $age");
+ 
+ //this is ternory for the multiple condition.
+ age<5 ?print("you are not elligble for school"):age>=5 && age<18?print("you are elligible for the school"):age>=18 && age<25?print("you are elleigble for the collage"):print("you are elligible for the university");
+
+
 
 }
