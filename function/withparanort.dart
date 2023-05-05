6e477fe25791bbd4259df8d7bb2435da9fbc0c56
LENV <technicalpriyanshutaru@gmail.com>
2023-05-05 10:42:26 +0530

    with return type no prata meter is done.

--- function/withparanort.dart
+++ function/withparanort.dart
@@ -21,3 +21,16 @@ void main(){
 
   helloprogrammer("Priyanshu");
 }
+
+
+// void add(int a, int b) {
+//   int sum = a + b;
+//   print("The sum is $sum");
+// }
+
+// void main() {
+//   int num1 = 10;
+//   int num2 = 20;
+
+//   add(num1, num2);
+// }
---
+++ function/withrtnopara.dart
@@ -0,0 +1,22 @@
+
+/*
+   
+   here we not provide the para meter but we provide the return type which 
+   is String type.
+   So we can say that this the type of with return type and no 
+   parameter of the function type.
+
+
+
+*/
+
+
+
+String name(){
+  return "Modi";
+}
+void main(){
+
+  String primeMinister = name();
+  print("India Prime Minister Name is $primeMinister");
+}
