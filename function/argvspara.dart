1d6473bd9d58667e95ceca68a4fb4bcd8a2b89ce
LENV <technicalpriyanshutaru@gmail.com>
2023-05-05 10:33:34 +0530

    no return type and no parameter is done.

---
+++ function/argvspara.dart
@@ -0,0 +1,10 @@
+
+void addition(int firstnumber,int secondNumber){
+  print("The sum is {$firstnumber+$secondNumber}");
+
+  //here the firstnumber and secondnumber is the parameter in the function so don't be confused in this section.
+}
+void main(){
+
+  addition(20, 17);// these are the arguments
+}
---
+++ function/noparanort.dart
@@ -0,0 +1,17 @@
+
+// here teh no parameter in the function and also no return type of the function
+// so we can say that this is the example of no return type and no parameter type of function.
+
+
+
+void primeMinisterName(){
+
+  print("India primeminister name is Modi");
+
+}
+
+void main(){
+
+  primeMinisterName();
+
+}
