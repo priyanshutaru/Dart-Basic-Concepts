ea7a8a481a6dc51e83d45c9b6e84ff059d1b07ef
LENV <technicalpriyanshutaru@gmail.com>
2023-05-05 10:37:22 +0530

    with parameter and no return type is done.

---
+++ function/withparanort.dart
@@ -0,0 +1,23 @@
+
+/*
+ 
+ here we give the string as the parameter but here no any return type
+ of the function so we can say that this the with parameter and no 
+ rerurn type of funcion.
+
+
+
+*/
+
+
+
+
+void helloprogrammer(String programmer){
+
+  print("Welcome $programmer");
+}
+
+void main(){
+
+  helloprogrammer("Priyanshu");
+}
