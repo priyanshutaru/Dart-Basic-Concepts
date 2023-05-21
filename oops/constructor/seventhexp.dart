92feab0521709285a2ffa6f9a5fbad70eddbd1aa
LENV <technicalpriyanshutaru@gmail.com>
2023-05-21 09:52:15 +0530

    Constructor With Named Parameters is done.

---
+++ oops/constructor/seventhexp.dart
@@ -0,0 +1,21 @@
+
+//Example 7: Constructor With Named Parameters
+
+class Chair {
+String? name;
+String? color;
+
+// Constructor
+Chair({this.name, this.color});
+
+// Method
+void display() {
+  print("Name: ${this.name}");
+  print("Color: ${this.color}");
+}
+}
+
+void main(){
+Chair chair = Chair(name: "Chair1", color: "Red");
+chair.display();
+}
