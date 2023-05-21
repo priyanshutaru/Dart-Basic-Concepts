7eac8b72e17d6516004701d0921eedfb059032ca
LENV <technicalpriyanshutaru@gmail.com>
2023-05-21 10:19:00 +0530

    exp two is done.

---
+++ oops/constructor/defaultconstructor.dart
@@ -0,0 +1,21 @@
+
+class DefalutConstructor {
+
+  String?brand;
+  int?price;
+
+  DefalutConstructor(){
+    print("\n\nThis is default Constructor");
+  }
+
+
+
+
+  
+}
+
+void main(){
+
+  DefalutConstructor laptopone = DefalutConstructor();
+
+}
